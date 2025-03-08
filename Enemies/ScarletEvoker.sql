d1e3190b9ea501a5eb4be28e7ff4e61578893a08
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-03-08 13:03:02 -0500

    Update ScarletEvoker.sql

--- Enemies/ScarletEvoker.sql
+++ Enemies/ScarletEvoker.sql
@@ -37,3 +37,10 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (601008, 0, 2, 0, 0, 0, 100, 0, 0, 1000, 3000, 3500, 0, 0, 11, 42834, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Evoker - In Combat - Cast \'Fireball\''),
 (601008, 0, 3, 0, 0, 0, 100, 0, 4000, 9000, 13000, 18000, 0, 0, 11, 42926, 0, 0, 0, 0, 0, 5, 30, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Evoker - In Combat - Cast \'Flamestrike\''),
 (601008, 0, 4, 0, 2, 0, 100, 1, 0, 15, 0, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Evoker - Between 0-15% Health - Flee For Assist (No Repeat)');
+
+DELETE FROM `creature_text` WHERE (`CreatureID` = 601008);
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(601008, 0, 0, 'You carry the taint of the Scourge. Prepare to enter the Twisting Nether.', 12, 7, 100, 0, 0, 0, 0, 0, 'Scarlet Evoker'),
+(601008, 0, 1, 'There is no escape for you. The Crusade shall destroy all who carry the Scourge\'s taint.', 12, 7, 100, 0, 0, 0, 0, 0, 'Scarlet Evoker'),
+(601008, 0, 2, 'The Light condemns all who harbor evil. Now you will die!', 12, 7, 100, 0, 0, 0, 0, 0, 'Scarlet Evoker'),
+(601008, 0, 3, 'The Scarlet Crusade shall smite the wicked and drive evil from these lands!', 12, 7, 100, 0, 0, 0, 2628, 0, 'Scarlet Evoker');
